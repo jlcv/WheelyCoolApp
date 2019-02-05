d290f7e23731479c1f637ef10ec16b8507212a82
jlcv <jlcv@github.com>
2019-02-05 14:12:43 -0600

    Initial Commit

---
+++ WheelyCoolApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WheelyCoolApp
+//
+//  Created by jChavez17 on 2/5/19.
+//  Copyright © 2019 jChavez17. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ WheelyCoolAppTests/WheelyCoolAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  WheelyCoolAppTests.swift
+//  WheelyCoolAppTests
+//
+//  Created by jChavez17 on 2/5/19.
+//  Copyright © 2019 jChavez17. All rights reserved.
+//
+
+import XCTest
+@testable import WheelyCoolApp
+
+class WheelyCoolAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
