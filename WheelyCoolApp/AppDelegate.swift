ec139010259d42146e7198d28b01aa35068336bd
jlcv <jlcv@github.com>
2019-02-05 20:36:34 -0600

    Initial commit

--- WheelyCoolApp/AppDelegate.swift
+++ WheelyCoolApp/AppDelegate.swift
@@ -14,10 +14,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        setupInitialViewController()
         return true
+    }
+    
+    private func setupInitialViewController() {
+        let initialNavigationController = UINavigationController.init(rootViewController: InputViewController())
+        window = UIWindow(frame:UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.rootViewController = initialNavigationController
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
